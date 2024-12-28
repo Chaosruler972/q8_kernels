f36b646ed0e001f0a10ed94b56b28baa82e886fc
Chaosruler <zmelumian@gmail.com>
2024-12-28 16:32:39 +0200

    Forcing warp sync builtings

--- csrc/amdsupport/amdsupport.h
+++ csrc/amdsupport/amdsupport.h
@@ -1,10 +1,11 @@
 #pragma once
 #include <stdint.h>
 
-#ifdef __HIPCC__
 #ifndef HIP_ENABLE_WARP_SYNC_BUILTINS
 #define HIP_ENABLE_WARP_SYNC_BUILTINS
 #endif // HIP_ENABLE_WARP_SYNC_BUILTINS
+
+#ifdef __HIPCC__
 #include <hip/hip_runtime.h>
 #include "nvcc_to_rocm.h"
 typedef uint64_t shfl_mask_t;
